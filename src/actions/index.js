c2f7d2db3227b471f950cc21b4c1996710650f88
Umair <ubaig517@gmail.com>
2018-08-18 16:14:55 -0400

    Created a redux action to retrieve user input and make call to weather API

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '4ae2b1db5f1dbd12702b469e584c8eb2';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
